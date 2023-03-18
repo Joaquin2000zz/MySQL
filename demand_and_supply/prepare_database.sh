0560166e7cdff426a2f7c7d2830da3bac511266b
Joaquin2000zz <joaquin2000zz@gmail.com>
2023-03-18 16:06:30 -0300

    changed place

--- demand_and_supply/prepare_database.sh
+++ demand_and_supply/prepare_database.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-read -p "Path: " path
-read -p "File: " file
 read -p "Database: " db
-read -p "Table: " table
 read -p "Host: " host
 
 echo "DROP DATABASE IF EXISTS $db;" | mysql -h $host -u root -p
 echo "CREATE DATABASE IF NOT EXISTS $db;" | mysql -h $host -u root -p
 cat prepare_tables.sql | mysql -h $host -u root -p $db
+cat set_triggers.sql | mysql -h $host -u root -p $db
+cat set_procedures.sql | mysql -h $host -u root -p $db
